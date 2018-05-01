4d020678b670245ac437d4536acb198d36535e70
Jean Lambert <jeeaan1702@gmail.com>
2018-05-01 12:22:43 -0700

    spread operator

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,23 +3,36 @@ import { SET_CART_ITEMS, ADD_TO_CART, DELETE_FROM_CART } from "../actions";
 function reducers(state = {items: []}, action) {
     switch(action.type) {
         case SET_CART_ITEMS: {
-            return Object.assign({}, state, {
+            return {
                 type: SET_CART_ITEMS,
                 items: action.items,
-            });
+            };            
+            // Object.assign({}, state, {
+            //     type: SET_CART_ITEMS,
+            //     items: action.items,
+            // });
         }
         case ADD_TO_CART: {
-            return Object.assign({}, state, {
+            return {
                 type: ADD_TO_CART,
-                items: [ ...state.items, action.item],
-            });
+                items: [...state.items, action.item],
+            };
+            
+            // Object.assign({}, state, {
+            //     type: ADD_TO_CART,
+            //     items: [ ...state.items, action.item],
+            // });
         }
         case DELETE_FROM_CART: {
             const items = state.items.filter(item => item.id !== action.id);
-            return Object.assign({}, state, {
+            return {
                 type: DELETE_FROM_CART,
                 items: items,
-            });
+            };            
+            // Object.assign({}, state, {
+            //     type: DELETE_FROM_CART,
+            //     items: items,
+            // });
         }
         default: {
             return state;
